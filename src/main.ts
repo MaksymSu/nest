c028c1700360eaa2417ba86224689515ccd8d52a
Maksym Sukhomlynov <lbstalker512@gmail.com>
2022-06-20 10:36:46 +0300

    inst swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
 
 const port = process.env.PORT || 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+    const config = new DocumentBuilder()
+        .setTitle('RBAC auth api')
+        .setDescription('RBAC administrator')
+        .setVersion('1.0.0')
+        .addTag('LuckyBot')
+        .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('/api/docs', app, document);
+
   await app.listen(port, () => console.log(`Server started on port ${port}`));
 }
 bootstrap();
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,14 +2,16 @@ import {UsersService} from "./users.service";
 import {User} from "./users.model";
 import {Body, Controller, Get, Post} from "@nestjs/common";
 import {CreateUserDto} from "./dto/create-user.dto";
+import {ApiOperation, ApiResponse} from "@nestjs/swagger";
 
 
-@Controller('users')
+@Controller('api/users')
 export class UsersController {
 
     constructor(private usersService: UsersService) {}
 
-
+    @ApiOperation({summary: 'Getting all users'})
+    @ApiResponse({status: 200, type: User})
     @Get()
     getAll() {
         return this.usersService.getAllUsers();
